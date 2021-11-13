be634c83e6c1cc18a82cc43e0c2ca5111edb14d7
Sergey Savchuk <savchuk.sergey@gmail.com>
2021-11-13 08:03:20 +0200

    exporting

--- src/ecc.asm
+++ src/ecc.asm
@@ -10,16 +10,29 @@ proc DllEntryPoint hinstDLL,fdwReason,lpvReserved
         ret
 endp
 
-proc BigInteger128_Rand result
+macro BigIntegerWin64RndProc bits {
+proc BigIntegerRnd_#bits result
         push     rax
         mov      rax, rcx
-        call     bi128_rnd
+        call     __bi_rnd_#bits
         pop      rax
+        ret
 endp
+}
+
+macro BigIntegerWin64Procs bits {
+        BigIntegerWin64RndProc bits
+}
+
+BigIntegerWin64Procs 128
+BigIntegerWin64Procs 256
+BigIntegerWin64Procs 512
 
 include 'math/bigint.inc'
 
 section '.edata' export data readable
 
   export 'ecc.dll',\
-         BigInteger128_Rand, 'BigInteger128_Rand'
+         BigIntegerRnd_128, 'bi128_rnd', \
+         BigIntegerRnd_256, 'bi256_rnd', \
+         BigIntegerRnd_512, 'bi512_rnd'
