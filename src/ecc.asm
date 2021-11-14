98ad15ac6fed7fdc9a2bfcf800c86bf4efaf76d2
Sergey Savchuk <savchuk.sergey@gmail.com>
2021-11-14 13:37:45 +0200

    refactoring

--- src/ecc.asm
+++ src/ecc.asm
@@ -14,17 +14,25 @@ endp
 include 'math/bigint.inc'
 include 'ecc_win64_api.inc'
 
-
-
 section '.edata' export data readable
 
   export 'ecc.dll',\
+         BigIntegerAdd_128, 'bi128_add', \
+         BigIntegerAdd_256, 'bi256_add', \
+         BigIntegerAdd_512, 'bi512_add', \
+\
          BigIntegerCmp_128, 'bi128_cmp', \
          BigIntegerCmp_256, 'bi256_cmp', \
          BigIntegerCmp_512, 'bi512_cmp', \
+\
          BigIntegerRnd_128, 'bi128_rnd', \
          BigIntegerRnd_256, 'bi256_rnd', \
          BigIntegerRnd_512, 'bi512_rnd', \
+\
+         BigIntegerSub_128, 'bi128_sub', \
+         BigIntegerSub_256, 'bi256_sub', \
+         BigIntegerSub_512, 'bi512_sub', \
+\
          BigIntegerZero_128, 'bi128_zero', \
          BigIntegerZero_256, 'bi256_zero', \
          BigIntegerZero_512, 'bi512_zero'
