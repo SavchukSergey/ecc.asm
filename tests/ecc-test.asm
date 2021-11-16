a82bc6138173ff6d7302fb13cd53011538dd555b
Sergey Savchuk <savchuk.sergey@gmail.com>
2021-11-16 11:46:58 +0200

    test performance

--- tests/ecc-test.asm
+++ tests/ecc-test.asm
@@ -32,9 +32,30 @@ start:
 .bi512_fixture_name db 'bi512', 0
 
 include '../src/console/enable_vt_processing.inc'
+include '../src/console/write_u64.inc'
+
+get_cpu_timestamp:
+        push    rdx
+        rdtsc
+        shl     rdx, 32
+        or      rdx, rax
+        mov     rdx, rax
+        pop     rdx
+        ret
 
 run_test_fixture:
         push    rax rbx rsi
+virtual at rsp
+  .locals_start:
+  .test rq 1
+  .test_proc rq 1
+  .test_start_timestamp rq 1
+  .test_end_timestamp rq 1
+  .test_result rq 1
+  .locals_end:
+end virtual
+        enter   .locals_end - .locals_start, 0
+        mov     [.test], rax
 
         call    Console_WriteString
 
@@ -44,30 +65,39 @@ run_test_fixture:
         call    Console_WriteLine
 
 .loop:
+        mov     rax, [.test]
         mov     rbx, [rax]
         test    rbx, rbx
         jz      .end
-        add     rax, 8
+        mov     [.test_proc], rbx
+        add     qword [.test], 8
 
         mov     rsi, .test_prefix
         call    Console_WriteString
 
-        mov     rsi, rax
+        mov     rsi, [.test]
         call    Console_WriteString
 .test_name_end_loop:
+        mov     rax, [.test]
         cmp     byte [rax], 0
         je      .test_name_end
-        inc     rax
+        inc     [.test]
         jmp     .test_name_end_loop
 .test_name_end:
-        inc     rax
+        inc     [.test]
 
         mov     rsi, .test_suffix
         call    Console_WriteString
 
-        push    rax
-        call    rbx
-        jnc      .test_ok
+        call    get_cpu_timestamp
+        mov     [.test_start_timestamp], rax
+        call    [.test_proc]
+        setc    byte [.test_result]
+        call    get_cpu_timestamp
+        mov     [.test_end_timestamp], rax
+
+        cmp     byte [.test_result], 0x00
+        je      .test_ok
 .test_fail:
         mov     rsi, .failed
         call    Console_WriteString
@@ -77,11 +107,18 @@ run_test_fixture:
         call    Console_WriteString
         jmp     .test_end
 .test_end:
+        mov     rsi, .time_prefix
+        call    Console_WriteString
+        mov     rax, [.test_end_timestamp]
+        sub     rax, [.test_start_timestamp]
+        call    Console_WriteUInt64
+        mov     rsi, .time_suffix
+        call    Console_WriteString
         call    Console_WriteLine
-        pop     rax
 
         jmp     .loop
 .end:
+        leave
         pop     rsi rbx rax
         ret
 
@@ -90,6 +127,8 @@ run_test_fixture:
 .test_suffix db ' test', 0
 .failed db TEXT_COLOR_ERROR, ' FAILED', TEXT_COLOR_DEFAULT, 0
 .passed db TEXT_COLOR_SUCCESS, ' PASSED', TEXT_COLOR_DEFAULT, 0
+.time_prefix db ' (', 0
+.time_suffix db ' cpu ticks)', 0
 
 include '../src/math/bigint.inc'
 include 'math/bigint.tests.inc'
