cc7796b8c9dd2b0641dbbec126c439b14aa7c453
Serhii Savchuk <savchuk.sergey@gmail.com>
2024-11-13 19:26:19 +0200

    projective point

--- src/ecc.asm
+++ src/ecc.asm
@@ -378,6 +378,11 @@ section '.edata' export data readable
          ec_projective_point_256_add_assign, 'ec_projective_point_256_add_assign', \
          ec_projective_point_256_double, 'ec_projective_point_256_double', \
          ec_projective_point_256_double_assign, 'ec_projective_point_256_double_assign', \
+         ec_projective_point_256_sub, 'ec_projective_point_256_sub', \
+         ec_projective_point_256_sub_assign, 'ec_projective_point_256_sub_assign', \
          ec_projective_point_256_shl, 'ec_projective_point_256_shl', \
+         ec_projective_point_256_shl_assign, 'ec_projective_point_256_shl_assign', \
+         ec_projective_point_256_mul, 'ec_projective_point_256_mul', \
+         ec_projective_point_256_negate, 'ec_projective_point_256_negate', \
          ec_projective_point_256_from_affine_point, 'ec_projective_point_256_from_affine_point', \
          ec_projective_point_256_to_affine_point, 'ec_projective_point_256_to_affine_point'
